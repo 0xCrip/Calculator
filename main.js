f3686c1863f3cfcb3b6a2d524802bc187671edaa
Crip <88676108+0xCrip@users.noreply.github.com>
2024-06-11 00:52:44 +0100

    Almost Complete - Some bugs need fixed | And styles need finalised

--- main.js
+++ main.js
@@ -1,54 +1,79 @@
 function addNum(num1, num2) {
-    let output = num1 + num2
-    console.log(output)
+    let res = num1 + num2
+    outputBox.textContent = outputBox.textContent + " = " + res
 }
 
 function subtractNum(num1, num2) {
-    let output = num1 - num2
-    console.log(output)
+    let res = num1 - num2
+    outputBox.textContent = outputBox.textContent + " = " + res
 }
 
 function multiplyNum(num1, num2) {
-    let output = num1 * num2
-    console.log(output)
+    let res = num1 * num2
+    outputBox.textContent = outputBox.textContent + " = " + res
 }
 
 function divideNum(num1, num2) {
-    let output = num1 / num2
-    console.log(output)
+    let res = num1 / num2
+    outputBox.textContent = outputBox.textContent + " = " + res
 }
 
-function operate(operator, num1, num2) {
-    console.log(operator)
+let inputCount = 0
+let oper
+let num1 = 0 
+let num2 = 0
 
+function operate(operator, num1, num2) {
     if (operator == "+") {
         addNum(num1, num2);
     } else if (operator == "-") {
         subtractNum(num1, num2);
-    }else if (operator == "*") {
+    }else if (operator == "x") {
+        operator = "*";
         multiplyNum(num1, num2);
     } else {
         divideNum(num1, num2)
     };
 }
 
-// operate(oper, num1, num2)
-let inputCount = 0
-let oper
-let num1
-let num2
+// This contains all the events for the buttons
+const outputBox = document.querySelector('#output');
+const numberButtons = document.querySelectorAll('#numbers button');
+const operButtons = document.querySelectorAll('#plus, #minus, #multiply, #divide');
+const btnClear = document.querySelector('#clear');
+const btnRes = document.querySelector('#result');
 
-console.log(num1)
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        inputCount++
+        output.textContent = outputBox.textContent + button.textContent
+        if (inputCount == 1) {
+            num1 = parseInt(button.textContent)
+        } else {
+            num2 = parseInt(button.textContent)
+        }
+    })
+})
 
-function numCheck() {
-    if (inputCount == 3) {
-        operate(oper, num1, num2);
-    } else {
-        return
-    }
-}
+operButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        oper = button.textContent;
+        outputBox.textContent = outputBox.textContent + button.textContent;
+    })
+})
 
-const btnPlus = document.querySelector("#plus");
-// btnPlus.addEventListener('click', () => {
+btnClear.addEventListener('click', () => {
+    inputCount = 0
+    oper = undefined
+    num1 = undefined
+    num2 = undefined
+    outputBox.textContent = ""
+    console.log("Calculator Cleared")
+})
 
-// })
+btnRes.addEventListener('click', () => {
+    console.log(oper + " " + num1 + " " + num2)
+
+    
+    operate(oper, num1, num2)
+})
